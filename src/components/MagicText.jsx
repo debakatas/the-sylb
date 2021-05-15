691b67502074b6cd511e5bfe5ce250fe2be8c4a3
Nicolas M. Pardo <nicolasm@nikodermus.media>
2021-05-15 12:50:27 -0500

    Update live workshop

---
+++ src/components/MagicText.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MagicText = ({text: propText, speed=70}) => {
+    const [text, setText] = useState('');
+    const [step, setStep] = useState(0);
+
+
+
+
+    return (  );
+}
+
+export default MagicText;
